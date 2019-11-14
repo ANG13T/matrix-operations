6707d154d737f0213ad5a902467b0f4f81cf845b
angelina-tsuboi <angelina.t1832@gmail.com>
2019-11-13 18:56:11 -0800

    Added Transpose Matrix

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ $('#selectOperation').change(function(){
    else if(operator === 'Subtract'){
        $('#operatorSymbol').html('-')
    }
+
+   else if(operator === 'Transpose'){
+    $('#operatorSymbol').html('')
+   }
    else{
        $('#operatorSymbol').html('x')
    }
@@ -31,6 +35,12 @@ $('#calculateButton').click(function(){
         console.log('The operator is the multiplication')
     }
 
+    else if(operator === 'Transpose'){
+        matrix1 = parseMatrix(matrix1);
+        console.log(transposingMatrix(matrix1))
+        var result = printMatrix(transposingMatrix(matrix1))
+    }
+
     else{
         console.log('Operation has not been selected')
     }
@@ -47,9 +57,14 @@ function addMatrix(m1, m2){
     let newMatrix = "";
     for(let i = 0; i < m1.length; i++){ //loops through rows
       let newRow = " "
+      m1[i] = m1[i].trim();
+
       let oldRow1 = m1[i].split(' ') //gets each element
       let oldRow2 = m2[i].split(' ')
         for(let j =0; j < oldRow1.length; j++){ //loops through elements
+            // oldRow1[j] = oldRow1[j].trim()
+            // oldRow2[j] = oldRow2[j].trim()
+           
           newRow += parseInt(oldRow1[j]) + parseInt(oldRow2[j]) + " "
         }
         newMatrix += newRow + "<br>"
@@ -64,6 +79,7 @@ function subtractMatrix(m1, m2){
     let newMatrix = "";
     for(let i = 0; i < m1.length; i++){ //loops through rows
         let newRow = " ";
+        m1[i] = m1[i].trim();
         let oldRow1 = m1[i].split(' ');
         let oldRow2 = m2[i].split(' ');
             for(var j = 0; j < oldRow1.length; j++){ //loops through elements
@@ -73,3 +89,38 @@ function subtractMatrix(m1, m2){
     }
     return newMatrix;
 }
+
+//Transpose Matrix Function
+function transposingMatrix(matrix){
+    let newMatrix = [];
+    for(var i = 0; i < matrix[0].length; i++){
+      newMatrix.push([]) //creates an array of empty arrays
+    }
+  
+    for(var i = 0; i < matrix.length; i++){
+  
+      for(var j = 0; j < matrix[0].length; j++){
+         newMatrix[j].push(matrix[i][j])     
+      }
+    }
+    return newMatrix;
+  }
+  
+  //print the matrix stated in the above
+  function printMatrix(matrix){
+    for(var i = 0; i < matrix.length; i++){
+      matrix[i] = matrix[i].join(" ");
+    }
+  
+    matrix = matrix.join("<br>")
+    return matrix;
+  }
+
+  function parseMatrix(strMat){
+    var matrix = strMat.split("\n"); //splits it up based on new lines
+    for(var i = 0; i < matrix.length; i++){
+      matrix[i] = matrix[i].split(" ")
+    }
+    return matrix;
+  
+  }
